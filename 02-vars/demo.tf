639f43c7f0fe94de4dfb72c122b67d938264ca21
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-10-17 14:28:40 +0530

    var map Terraform push

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -10,6 +10,9 @@ output "sample-op1" {
     value = "Value is ${var.sample}"
 }
 
+# A variable can be accessed with or without ${} only if the variable is called. But in case if it is needed to add with some other strings then we need to use double
+# There is no concept of single quores in terraform.
+
 variable "number" {
     default = 100
 }
@@ -17,6 +20,8 @@ variable "number" {
 output "number" {
     value = var.number
 }
+
+#Declaring a list variable
 
 variable "ex-list" {
     default = [
@@ -29,6 +34,23 @@ variable "ex-list" {
     ]
 }
 
+# From the above list, you can see that data in list can be of any type indivudually. 
 output "ex-list-op" {
     value = "Welcome to ${var.ex-list[2]} batch 50, this contains ${var.ex-list[3]} chapters and the trainer is ${var.ex-list[5]}"
+}
+
+# Declaring a map variable
+
+variable "ex-map" {
+    default = {
+        Class = "DevOps",
+        Duration = 85,
+        Trainer = "Verma",
+        Batch = "0600AM"
+    }
+  
+}
+
+output "ex-map" {
+    value = "welcome to ${var.ex-map["Class"]} training and schedule of training is ${var.ex-map["Duration"]} hours and scheduled time is ${var.ex-map["Batch"]}"
 }
