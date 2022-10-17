3e1ca64e658081f8c1589a7cc95d3e1750833769
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-10-17 14:21:04 +0530

    var list Terraform push

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -17,3 +17,18 @@ variable "number" {
 output "number" {
     value = var.number
 }
+
+variable "ex-list" {
+    default = [
+        "AWS",
+        "GCP",
+        "DevOPs",
+        100,
+        true,
+        "Verma"
+    ]
+}
+
+output "ex-list-op" {
+    value = "Welcome to ${var.ex-list[2]} batch 50, this contains ${var.ex-list[3]} chapters and the trainer is ${var.ex-list[5]}"
+}
