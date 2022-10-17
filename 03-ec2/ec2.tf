89ae0025cb60a246bd9375404d9f2a4579dcacdf
nileshrajurkar1608 <nilesh.rajurkar@gmail.com>
2022-10-17 15:22:30 +0530

    EC2 with SG Terraform push

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -5,8 +5,40 @@ provider "aws" {
 resource "aws_instance" "demo" {
   ami           = "ami-0e59251addb8f9901"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
 
   tags = {
     Name = "HelloWorld"
+  }
+}
+
+output "Public IP" {
+    value = aws_instance.demo.public_ip
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
   }
 }
